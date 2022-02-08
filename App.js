016d206ca9b10be402ae774d45532c8051b360fc
Dorota Tomala <doforever@gmail.com>
2022-02-08 14:04:17 +0100

    Fetch user rooms data

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Navigation from './navigation/Navigation';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+import { TOKEN } from '@env';
+
+const httpLink = createHttpLink({
+  uri: 'https://chat.thewidlarzgroup.com/api/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: TOKEN ? `Bearer ${TOKEN}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <SafeAreaProvider>
+    <ApolloProvider client={client}>
       <Navigation />
       <StatusBar />
-    </SafeAreaProvider>
+    </ApolloProvider>
   );
 }
--- screens/Rooms.js
+++ screens/Rooms.js
@@ -1,11 +1,33 @@
-import { StyleSheet, View, Button } from 'react-native';
+import { StyleSheet, View, Button, Text } from 'react-native';
+import {
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const USER_ROOMS = gql`
+query getUserRooms {
+    usersRooms {
+    rooms {
+        id
+        name
+      }
+    }
+  }
+`;
 
 export default function Rooms({navigation}) {
+  const { loading, error, data } = useQuery(USER_ROOMS);
+
+  if(data) console.log('Data', data);
+
+  if (loading) return <View><Text>Loading...</Text></View>;
+  if (error) return <View><Text>Error :-(</Text></View>;
+
   return (
     <View style={styles.container}>
       <Button 
-        title='Go to Chat'
-        onPress={() => navigation.navigate('Chat', {title: 'Widlarz'})}
+        title='Chat'
+        onPress={() => navigation.navigate('Chat', { title: 'Chat name'})}
       />
     </View>
   );
